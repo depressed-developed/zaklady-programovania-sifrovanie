adee69c86189b83f318ed422e5b1356374f5a86a
Marek Kadlec <101722466+depressed-developed@users.noreply.github.com>
2024-11-03 14:14:19 +0100

    updated rot13

--- rot13.py
+++ rot13.py
@@ -1,28 +1,45 @@
+def rot13_encode_decode(message):
+    result = []
 
-def encode(message):
-    letters = [x for x in message]
-    encoded_message = ""
-    for i in range(len(message)):
-        if letters[i] != " ":
-            if(ord(message[i]) + 13) >= 97 and (ord(message[i]) + 13) <= 122:
-                shifted_letter = chr(ord(message[i]) + 13)
-                encoded_message += shifted_letter
-            elif (ord(message[i]) + 13) > 122:
-                shifted_letter = chr(96 + (13 - (122 - ord(message[i]))))
-                encoded_message += shifted_letter
-            else: 
-                shifted_letter = chr(122 - (abs(13) - (ord(message[i]) - 97)))
-                encoded_message += shifted_letter
+    for char in message:
+        # Check if it's an uppercase letter
+        if 'A' <= char <= 'Z':
+            # Rotate within uppercase letters
+            rotated_char = chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
+            result.append(rotated_char)
+        # Check if it's a lowercase letter
+        elif 'a' <= char <= 'z':
+            # Rotate within lowercase letters
+            rotated_char = chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
+            result.append(rotated_char)
         else:
-            encoded_message += " "
-    return encoded_message
+            # Leave non-alphabet characters as they are
+            result.append(char)
+    
+    # Join list into final string
+    return ''.join(result)
+
 
 while True:
     try:
-        user_input = str(input("Please enter a string to decode:"))
-        print(encode(user_input))
-        #print(decode(encode(user_input)))
-    except ValueError as e:
-        print(f"Input must be of type string: {e}")
+        user_input = str(input("Do you wish do encode or decode a message? e/d "))
+        if (user_input == "e"):
+            try:
+                message_to_encode = str(input("Please enter a message to encode: "))
+                encoded_message = rot13_encode_decode(message_to_encode)
+                print(f"Encoded Message: {encoded_message}")
+            except Exception as e:
+                print(f"An unexpected error has occured: {e}")
+        elif (user_input == "d"):
+            try:
+                message_to_decode = str(input("Please enter a message to decode: "))
+                decoded_message = rot13_encode_decode(message_to_decode)
+                print(f'Decoded message: {decoded_message}')
+            except Exception as e:
+                print(f"An unexpected error has occured: {e}")
+        else:
+            print("Enter 'e' to encode or 'd' to decode")
+    except ValueError:
+        print(f'Message must be of type string')
     except Exception as e:
-        print(f"Unexpected error has occured: {e}")
+        print(f'An unexpected error has occured: {e}')
